6c6d255267496117ac52503644b5d0245b5b147a
kanata0510 <kawakana0510@gmail.com>
2024-12-25 09:57:05 +0900

    identify project object id

--- Assets/AttachHelper/Editor/AttachHelper.cs
+++ Assets/AttachHelper/Editor/AttachHelper.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
@@ -10,87 +9,15 @@ namespace AttachHelper.Editor
 {
     public class AttachHelper : EditorWindow
     {
-        public class PropertyComparer : IEqualityComparer<UniquePropertyInfo>
+        public class UniqueProperty
         {
-            public bool Equals(UniquePropertyInfo x, UniquePropertyInfo y)
-            {
-                if (ReferenceEquals(x, null)) return false;
-                if (ReferenceEquals(y, null)) return false;
-                return Equals(x.index, y.index) && Equals(x.propertyPath, y.propertyPath) && Equals(x.sceneName, y.sceneName) && Equals(x.GetHierarchy(), y.GetHierarchy());
-            }
-            
-            public int GetHashCode(UniquePropertyInfo obj)
-            {
-                return HashCode.Combine(obj.index, obj.propertyPath, obj.sceneName, obj.GetHierarchy());
-            }
-        }
+            public string GlobalObjectIdString;
+            public SerializedProperty Property;
         
-        public class UniqueProperty : UniquePropertyInfo
-        {
-            public SerializedProperty SerializedProperty;
-            public GameObject GameObject;
-        
-            public UniqueProperty(Component component, SerializedProperty serializedProperty) : base(component, serializedProperty)
+            public UniqueProperty(string globalObjectIdString, SerializedProperty property)
             {
-                SerializedProperty = serializedProperty.Copy();
-                GameObject = component.gameObject;
-            }
-        }
-    
-        public class UniquePropertyInfo
-        {
-            public int index;
-            public string propertyPath;
-            public string sceneName;
-            public List<string> objNames;
-        
-            public UniquePropertyInfo(Component component, SerializedProperty serializedProperty)
-            {
-                var components = component.GetComponents<Component>();
-                for (int i = 0; i < components.Length; i++)
-                {
-                    if (components[i].Equals(component))
-                    {
-                        index = i;
-                        break;
-                    }
-                }
-                objNames = new List<string>();
-                AddRecursiveParent(component.transform);
-                propertyPath = serializedProperty.propertyPath;
-                sceneName = SceneManager.GetActiveScene().name;
-            }
-        
-            public UniquePropertyInfo(int index, string propertyPath, string sceneName, string hierarchy)
-            {
-                this.index = index;
-                this.propertyPath = propertyPath;
-                this.sceneName = sceneName;
-                objNames = new List<string>();
-                foreach (string objName in hierarchy.Split(" > "))
-                {
-                    objNames.Add(objName);
-                }
-            }
-            
-            private void AddRecursiveParent(Transform transform)
-            {
-                if (transform is null) return;
-                
-                objNames.Add(transform.name);
-                AddRecursiveParent(transform.parent);
-            }
-
-            public string GetHierarchy()
-            {
-                StringBuilder stringBuilder = new StringBuilder();
-                for (int i = 0; i < objNames.Count - 1; i++)
-                {
-                    stringBuilder.Append(objNames[i]);
-                    stringBuilder.Append(" > ");
-                }
-                stringBuilder.Append(objNames[^1]);
-                return stringBuilder.ToString();
+                GlobalObjectIdString = globalObjectIdString;
+                Property = property.Copy();
             }
         }
     
@@ -102,12 +29,12 @@ namespace AttachHelper.Editor
         /// <summary>
         /// showに登録されたやつを検索するためのやつ
         /// </summary>
-        private static HashSet<UniquePropertyInfo> showcomp = new HashSet<UniquePropertyInfo>(new PropertyComparer());
+        private static HashSet<string> showcomp = new HashSet<string>();
     
         /// <summary>
         /// Noneだけどそれでいいから無視するやつ。Noneじゃなくなってもそのまま。
         /// </summary>
-        private static HashSet<UniquePropertyInfo> ignores = new HashSet<UniquePropertyInfo>(new PropertyComparer());
+        private static HashSet<string> ignores = new HashSet<string>();
 
         private Vector2 scrollPosition = Vector2.zero;
     
@@ -140,7 +67,7 @@ namespace AttachHelper.Editor
         {
             foreach (UniqueProperty serializedObj in show)
             {
-                if (ignores.Contains(serializedObj)) continue;
+                if (ignores.Contains(serializedObj.GlobalObjectIdString)) continue;
                 return true;
             }
             return false;
@@ -179,10 +106,7 @@ namespace AttachHelper.Editor
             int ignoreCount = int.Parse(EditorUserSettings.GetConfigValue("ignoreCount"));
             for (int i = 0; i < ignoreCount; i++)
             {
-                EditorUserSettings.SetConfigValue($"propertyPath{i}", null);
-                EditorUserSettings.SetConfigValue($"index{i}", null);
-                EditorUserSettings.SetConfigValue($"sceneName{i}", null);
-                EditorUserSettings.SetConfigValue($"hierarchy{i}", null);
+                EditorUserSettings.SetConfigValue($"globalObjectIdString{i}", null);
             }
             EditorUserSettings.SetConfigValue("ignoreCount", null);
         }
@@ -197,23 +121,17 @@ namespace AttachHelper.Editor
             int ignoreCount = int.Parse(EditorUserSettings.GetConfigValue("ignoreCount"));
             for (int i = 0; i < ignoreCount; i++)
             {
-                string propertyPath = EditorUserSettings.GetConfigValue($"propertyPath{i}");
-                int index = int.Parse(EditorUserSettings.GetConfigValue($"index{i}"));
-                string sceneName = EditorUserSettings.GetConfigValue($"sceneName{i}");
-                string hierarchy = EditorUserSettings.GetConfigValue($"hierarchy{i}");
-                ignores.Add(new UniquePropertyInfo(index, propertyPath, sceneName, hierarchy));
+                string globalObjectId = EditorUserSettings.GetConfigValue($"globalObjectIdString{i}");
+                ignores.Add(globalObjectId);
             }
         }
         
-        private static void AddIgnore(UniquePropertyInfo uniquePropertyInfo)
+        private static void AddIgnore(string uniquePropertyInfo)
         {
             ignores.Add(uniquePropertyInfo);
         
             int ignoreCount = int.Parse(EditorUserSettings.GetConfigValue("ignoreCount"));
-            EditorUserSettings.SetConfigValue($"propertyPath{ignoreCount}", uniquePropertyInfo.propertyPath);
-            EditorUserSettings.SetConfigValue($"index{ignoreCount}", uniquePropertyInfo.index.ToString());
-            EditorUserSettings.SetConfigValue($"sceneName{ignoreCount}", uniquePropertyInfo.sceneName);
-            EditorUserSettings.SetConfigValue($"hierarchy{ignoreCount}", uniquePropertyInfo.GetHierarchy());
+            EditorUserSettings.SetConfigValue($"globalObjectIdString{ignoreCount}", uniquePropertyInfo);
             EditorUserSettings.SetConfigValue("ignoreCount", (ignoreCount + 1).ToString());
         }
     
@@ -238,13 +156,14 @@ namespace AttachHelper.Editor
                     var serializedProp = serializedObj.GetIterator();
                     while (serializedProp.NextVisible(true))
                     {
-                        var uniqueProperty = new UniqueProperty(component, serializedProp);
+                        GlobalObjectId globalObjectId = GlobalObjectId.GetGlobalObjectIdSlow(component);
+                        string globalObjectIdString = globalObjectId.ToString();
                         if (serializedProp.propertyType != SerializedPropertyType.ObjectReference) continue;
                         if (serializedProp.objectReferenceValue != null) continue;
-                        if (showcomp.Contains(uniqueProperty)) continue;
-                        
-                        show.Add(uniqueProperty);
-                        showcomp.Add(uniqueProperty);
+                        if (showcomp.Contains(globalObjectIdString)) continue;
+
+                        show.Add(new UniqueProperty(globalObjectIdString, serializedProp));
+                        showcomp.Add(globalObjectIdString);
                     }
                 }
             }
@@ -266,26 +185,36 @@ namespace AttachHelper.Editor
                 scrollPosition = scrollViewScope.scrollPosition;
                 foreach (var serializedObj in show)
                 {
-                    if (ignores.Contains(serializedObj)) continue;
-                    if (serializedObj.GameObject == null) continue;
-                    var serializedProp = serializedObj.SerializedProperty;
-
-                    using (new EditorGUILayout.HorizontalScope())
+                    if (ignores.Contains(serializedObj.GlobalObjectIdString)) continue;
+                    if (GlobalObjectId.TryParse(serializedObj.GlobalObjectIdString, out GlobalObjectId globalObjectId))
                     {
-                        if (GUILayout.Button("Inspect", GUILayout.Width(100)))
+                        var obj = GlobalObjectId.GlobalObjectIdentifierToObjectSlow(globalObjectId);
+                        if (obj == null) continue;
+                        var serializedProp = serializedObj.Property;
+                        using (new EditorGUILayout.HorizontalScope())
                         {
-                            Selection.activeGameObject = serializedObj.GameObject;
-                        }
+                            Component component = obj as Component;
+                            if (component is null)
+                            {
+                                throw new NullReferenceException();
+                            }
+                            GameObject gameObj = component.gameObject;
+                            if (GUILayout.Button("Inspect", GUILayout.Width(100)))
+                            {
+                                Selection.activeGameObject = gameObj;
+                            }
 
-                        GUILayout.Label($"{serializedObj.GameObject.name} > {serializedObj.GameObject.GetComponents<Component>()[serializedObj.index].GetType()} > {serializedProp.displayName}", GUILayout.MinWidth(200));
+                            GUILayout.Label($"{gameObj.name} > {component.GetType()} > {serializedProp.displayName}", GUILayout.MinWidth(200));
 
-                        GUILayout.FlexibleSpace();
-                        EditorGUILayout.PropertyField(serializedProp, new GUIContent(GUIContent.none), true, GUILayout.MinWidth(150), GUILayout.MaxWidth(200), GUILayout.ExpandWidth(false));
-                        serializedProp.serializedObject.ApplyModifiedProperties();
-                        if (GUILayout.Button("Decide", GUILayout.Width(100)))
-                        {
-                            AddIgnore(serializedObj);
-                            AssetDatabase.SaveAssets();
+                            GUILayout.FlexibleSpace();
+                            EditorGUILayout.PropertyField(serializedProp, new GUIContent(GUIContent.none), true,
+                                GUILayout.MinWidth(150), GUILayout.MaxWidth(200), GUILayout.ExpandWidth(false));
+                            serializedProp.serializedObject.ApplyModifiedProperties();
+                            if (GUILayout.Button("Decide", GUILayout.Width(100)))
+                            {
+                                AddIgnore(serializedObj.GlobalObjectIdString);
+                                AssetDatabase.SaveAssets();
+                            }
                         }
                     }
                 }
@@ -297,10 +226,10 @@ namespace AttachHelper.Editor
                 {
                     foreach (var serializedObj in show)
                     {
-                        if (ignores.Contains(serializedObj)) continue;
-                        var serializedProp = serializedObj.SerializedProperty;
+                        if (ignores.Contains(serializedObj.GlobalObjectIdString)) continue;
+                        var serializedProp = serializedObj.Property;
                         if (serializedProp.objectReferenceValue != null) continue;
-                        AddIgnore(serializedObj);
+                        AddIgnore(serializedObj.GlobalObjectIdString);
                     }
                 
                     AssetDatabase.SaveAssets();
@@ -310,10 +239,10 @@ namespace AttachHelper.Editor
                 {
                     foreach (var serializedObj in show)
                     {
-                        if (ignores.Contains(serializedObj)) continue;
-                        var serializedProp = serializedObj.SerializedProperty;
+                        if (ignores.Contains(serializedObj.GlobalObjectIdString)) continue;
+                        var serializedProp = serializedObj.Property;
                         if (serializedProp.objectReferenceValue == null) continue;
-                        AddIgnore(serializedObj);
+                        AddIgnore(serializedObj.GlobalObjectIdString);
                     }
                 
                     AssetDatabase.SaveAssets();
@@ -324,9 +253,9 @@ namespace AttachHelper.Editor
             {
                 foreach (var serializedObj in show)
                 {
-                    if (ignores.Contains(serializedObj)) continue;
+                    if (ignores.Contains(serializedObj.GlobalObjectIdString)) continue;
                 
-                    AddIgnore(serializedObj);
+                    AddIgnore(serializedObj.GlobalObjectIdString);
                 }
             
                 AssetDatabase.SaveAssets();
